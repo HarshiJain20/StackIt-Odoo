442ff09f0264964a4f93ccae754e80f1aa3ac4bc
HarshiJain20 <159061902+HarshiJain20@users.noreply.github.com>
2025-07-12 13:51:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const MainLayout = () => {
   return (
     <div className="flex">
       {showSidebar && <LeftSidebar />}
-      <div className="flex-1 min-h-screen bg-white pt-8 px-6">
+      <div className="flex-1 min-h-screen bg-white pt-6 px-6">
         <Routes>
           <Route
             path="/"
